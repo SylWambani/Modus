0853bf88126ec70bf8a47bb87b63868874b1c103
SYLVIA WAMBANI <wambanisylvia2@gmail.com>
2026-04-10 07:44:35 +0300

    Link Procurement views to audit trail view

--- procurement/views.py
+++ procurement/views.py
@@ -3,32 +3,24 @@ from rest_framework.decorators import action, permission_classes
 from rest_framework.permissions import AllowAny, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly, IsAdminUser, IsAuthenticated
 from .permissions import CanViewSupplier, FullDjangoModelPermissions, IsAdminOrReadOnly, ViewCustomerHistoryPermission
 from .serializers import AddPurchaseOrderSerializer, PurchaseOrderItemSerializer, SupplierSerializer, ViewPurchaseOrderSerializer
+from audit.views import AuditModelViewSet
 from .models import PurchaseOrder, PurchaseOrderItem, Supplier
 
 
-class SupplierViewSet(ModelViewSet):
+class SupplierViewSet(AuditModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
     permission_classes = [IsAuthenticated, CanViewSupplier]
 
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user, updated_by=self.request.user)
-
-    def perform_update(self, serializer):
-        serializer.save(updated_by=self.request.user)
-
-
-
-
-class PurchaseOrderItemViewSet(ModelViewSet):
+class PurchaseOrderItemViewSet(AuditModelViewSet):
     queryset = PurchaseOrderItem.objects.all()
     serializer_class = PurchaseOrderItemSerializer
 
-class ViewPurchaseOrderViewSet(ModelViewSet):
+class ViewPurchaseOrderViewSet(AuditModelViewSet):
     queryset = PurchaseOrder.objects.all()
     serializer_class = ViewPurchaseOrderSerializer
 
-class AddPurchaseOrderViewSet(ModelViewSet):
+class AddPurchaseOrderViewSet(AuditModelViewSet):
     queryset = PurchaseOrder.objects.all()
     serializer_class = AddPurchaseOrderSerializer
 

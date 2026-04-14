465c9effa35f4cd99c0f5da27480445a1f9a6031
SYLVIA WAMBANI <wambanisylvia2@gmail.com>
2026-04-14 08:25:54 +0300

    Enforcing permissions to Purchase Orders and purchase orderitems

--- procurement/permissions.py
+++ procurement/permissions.py
@@ -1,29 +1,49 @@
 from rest_framework import permissions
+from rest_framework.permissions import DjangoModelPermissions
 
 
-class CanViewSupplier(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method == "GET":
-            return request.user.has_perm("procurement.view_supplier")
-        elif request.method == "POST":
-            return request.user.has_perm("procurement.add_supplier")
-        elif request.method == "DELETE":
-            return request.user.has_perm("procurement.delete_supplier")
-        elif request.method == "PATCH":
-            return request.user.has_perm("procurement.change_supplier")
+class CustomDjangoModelPermissions(DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
 
-        return False
+# class CanViewSupplier(permissions.BasePermission):
+#     def has_permission(self, request, view):
+#         if request.method == "GET":
+#             return request.user.has_perm("procurement.view_supplier")
+#         elif request.method == "POST":
+#             return request.user.has_perm("procurement.add_supplier")
+#         elif request.method == "DELETE":
+#             return request.user.has_perm("procurement.delete_supplier")
+#         elif request.method == ["PATCH", "PUT"]:
+#             return request.user.has_perm("procurement.change_supplier")
+
+#         return False
+    
+# class CanViewPurchaseOrder(permissions.BasePermission):
+#     def has_permission(self, request, view):
+#         if request.method == "GET":
+#             return request.user.has_perm("procurement.view_purchase_order")
+#         elif request.method == "POST":
+#             return request.user.has_perm("procurement.add_purchase_order")
+#         elif request.method == "DELETE":
+#             return request.user.has_perm("procurement.delete_purchase_order")
+#         elif request.method == ["PATCH", "PUT"]:
+#             return request.user.has_perm("procurement.change_purchase_order")
+
+#         return False
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
-
-
-class FullDjangoModelPermissions(permissions.DjangoModelPermissions):
-    def __init__(self) -> None:
-        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
 
 class ViewCustomerHistoryPermission(permissions.BasePermission):
     def has_permission(self, request, view):
--- procurement/views.py
+++ procurement/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet, GenericViewSet, ReadOnlyModelViewSet
 from rest_framework.decorators import action, permission_classes
 from rest_framework.permissions import AllowAny, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly, IsAdminUser, IsAuthenticated
-from .permissions import CanViewSupplier, FullDjangoModelPermissions, IsAdminOrReadOnly, ViewCustomerHistoryPermission
+from .permissions import CustomDjangoModelPermissions 
 from .serializers import AddPurchaseOrderSerializer, PurchaseOrderItemSerializer, SupplierSerializer, ViewPurchaseOrderSerializer
 from audit.views import AuditModelViewSet
 from .models import PurchaseOrder, PurchaseOrderItem, Supplier
@@ -10,17 +10,23 @@ from .models import PurchaseOrder, PurchaseOrderItem, Supplier
 class SupplierViewSet(AuditModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
-    permission_classes = [IsAuthenticated, CanViewSupplier]
+    permission_classes = [IsAuthenticated, CustomDjangoModelPermissions]
 
 class PurchaseOrderItemViewSet(AuditModelViewSet):
     queryset = PurchaseOrderItem.objects.all()
     serializer_class = PurchaseOrderItemSerializer
+    permission_classes = [IsAuthenticated, CustomDjangoModelPermissions]
+
 
 class ViewPurchaseOrderViewSet(AuditModelViewSet):
     queryset = PurchaseOrder.objects.all()
     serializer_class = ViewPurchaseOrderSerializer
+    permission_classes = [IsAuthenticated, CustomDjangoModelPermissions]
+
 
 class AddPurchaseOrderViewSet(AuditModelViewSet):
     queryset = PurchaseOrder.objects.all()
     serializer_class = AddPurchaseOrderSerializer
+    permission_classes = [IsAuthenticated, CustomDjangoModelPermissions]
+
 
